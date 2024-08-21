ec2485f81306e68c797578c1091b7cdde962f6e0
suraj-858 <sooraz858@gmail.com>
2024-08-21 10:52:52 +0545

    banner tsx error code fixed

--- medix_frontend_user/src/components/landing_page_components/Banner.tsx
+++ medix_frontend_user/src/components/landing_page_components/Banner.tsx
@@ -1,19 +1,11 @@
 
 import BooksStack from "../../assets/Images/website/library.jpg";
-// import Vector from "../../assets/Images/vector3.png";
 import { GrSecure } from "react-icons/gr";
 import { IoFastFood } from "react-icons/io5";
 import { GiFoodTruck } from "react-icons/gi";
 
 const Banner = () => {
-  // const bgImage = {
-  //   backgroundImage: `url(${Vector})`,
-  //   backgroundPosition: "center",
-  //   backgroundRepeat: "no-repeat",
-  //   backgroundSize: "cover",
-  //   height: "100%",
-  //   width: "100%",
-  // };
+
   return (
     <>
       <div className="min-h-[550px]">
